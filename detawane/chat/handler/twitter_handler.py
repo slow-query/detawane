793b78201b11948c6edbb9b7f98be17eb0e209a5
slow-query <450712+slow-query@users.noreply.github.com>
2020-05-22 16:57:42 +0900

    Remove unnecessary codes

--- detawane/chat/handler/twitter_handler.py
+++ detawane/chat/handler/twitter_handler.py
@@ -1,14 +1,12 @@
 import os
 import tweepy
 from .base_handler import BaseHandler
-from ...logger import get_local_logger
 
 class Client:
     def __init__(self):
         auth = tweepy.OAuthHandler(os.environ['TWITTER_CONSUMER_KEY'], os.environ['TWITTER_CONSUMER_SECRET'])
         auth.set_access_token(os.environ['TWITTER_ACCESS_KEY'], os.environ['TWITTER_ACCESS_SECRET'])
         self.api = tweepy.API(auth)
-        self._logger = get_local_logger(__name__)
 
     def tweet(self, text):
         self.api.update_status(text)
@@ -20,6 +18,5 @@ class TwitterHandler(BaseHandler):
         super().__init__(video)
 
     def __call__(self, message):
-        self._logger.info(f'[{self.video.channel.owner_name}][{message.published_at}][{message.name}] {message.text}')
         text = f'{self.video.channel.owner_name}が「{self.video.title}」に現れました。 {self.video.url}'
         self.CLIENT.tweet(text)
